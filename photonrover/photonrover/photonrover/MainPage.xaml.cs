5474a09eb40fa9df1bda3f428cfdbc65b96a9e5b
Stathis Votsis <stathisvotsis@gmail.com>
2018-02-01 00:06:27 +0200

    fdfd

--- photonrover/photonrover/photonrover/MainPage.xaml.cs
+++ photonrover/photonrover/photonrover/MainPage.xaml.cs
@@ -14,5 +14,38 @@ namespace photonrover
 		{
 			InitializeComponent();
 		}
-	}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            //var pass1 = usern.Text;
+            //var pass1 = MainPage.userna
+           var pass1 = username.Text;
+           var pass2 = password.Text;
+           username.Text = "";
+           password.Text = "";
+           LoginCloud(pass1, pass2);
+        }
+
+        async void LoginCloud(string st1, string st2)
+        {
+            var loginSuccess = await ParticleCloud.SharedInstance.LoginWithUserAsync(st1, st2);
+            if (loginSuccess)
+            {
+                await DisplayAlert("You are loggedin as", st1, "OK");
+                NewPage(st1, st2);
+
+            }
+            else
+            {
+                await DisplayAlert("Invalid user credentials: ", st1, "OK");
+            }
+
+        }
+
+        private void NewPage(string st3, string st4)
+        {
+            Application.Current.MainPage = new SecondPage();
+            //Application.Current.MainPage = new ListViewPage1();
+        }
+    }
 }
---
+++ photonrover/photonrover/photonrover/SecondPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Particle;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace photonrover
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SecondPage : ContentPage
+    {
+        ParticleDevice myDevice = null;
+        
+        public SecondPage()
+        {
+            InitializeComponent();
+            
+            Task1();
+        }
+
+        private void Button_ClickF(object sender, EventArgs e)
+        {
+            Control_F();
+        }
+
+        async void Task1()
+        {
+
+            List<ParticleDevice> devices = await ParticleCloud.SharedInstance.GetDevicesAsync();
+            foreach (ParticleDevice device in devices)
+            {
+                if (device.Name.Equals("Master2"))
+                    myDevice = device;
+            }
+
+
+        }
+
+        async void Control_F()
+        {
+            var response = await myDevice.CallFunctionAsync("control", "F-100");
+
+        }
+
+        async void Control_B()
+        {
+            var response = await myDevice.CallFunctionAsync("relayOff", "B-100");
+
+        }
+    }
+}
